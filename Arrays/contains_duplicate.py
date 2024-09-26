56a5c9b0260f9aa3209eca0f992f7d1da72b0daf
wlindor <westlindor@gmail.com>
2024-09-26 14:41:15 -0500

    remove dups

--- Arrays/contains_duplicate.py
+++ Arrays/contains_duplicate.py
@@ -13,3 +13,22 @@ Input: nums = [1, 2, 3, 4]
 
 Output: false
 """
+
+def has_duplicate(nums: list[int]) -> bool:
+    # edge case
+    if not nums:
+        return False
+    
+    hash_set = set()
+
+    for num in nums:
+        if num in hash_set:
+            return True
+        hash_set.add(num)
+    return False
+
+print(has_duplicate([1,2,3,4,5])) #False
+print(has_duplicate([1,2,3,3,5])) #True
+print(has_duplicate([1])) #False
+print(has_duplicate([2,2])) #True
+print(has_duplicate([])) #False
